e14796766b9852587c8c5f917a188672d5ed827c
HamidAliAPA103 <hamidva-apa103@code.edu.az>
2026-03-13 13:56:02 +0400

    ref out

---
+++ ref out/ref out/Program.cs
@@ -0,0 +1,57 @@
+﻿
+// 1ci mesele
+
+//int num1 = 2;
+//int num2 = 3;
+//static void Add(ref int num1 , ref int num2)
+//{
+//    int resoult = num1;
+//    num1 = num2;
+//    num2 = resoult;
+//}
+//Add(ref num1, ref num2);
+
+//Console.WriteLine(" num1 =" + num1); 
+//Console.WriteLine(" num2 =" + num2);
+
+
+//2ci mesele
+
+//int num1 = 5;
+//static void Plus(ref int num1)
+//{
+//    int eded = 10;
+//    eded += num1;
+//Console.WriteLine("cem :" + eded);
+//}
+//Plus(ref num1);
+
+
+//3cu mesele
+
+//Plus(out int resoult);
+//static void Plus(out int result)
+//{
+
+//    int num1 = 50;
+//    result = num1 / 10;
+
+//    Console.WriteLine("bolunen :" + result);
+//}
+
+//4cu mesele
+
+//static void FindMinMax(int a, int b, int c, out int min, out int max)
+//{
+//     min = a;
+//     max = a;
+//    if (b < a) min = b;
+//    if (c < a) min = c;
+//    if (b > a) max = b;
+//    if (c > a) max = c;
+//    Console.WriteLine($"{min} {max}");
+    
+//}
+//int min = 0;
+//int max = 0;
+//FindMinMax(35, 5, 4,out  min, out max);
